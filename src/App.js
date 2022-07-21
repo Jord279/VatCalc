01982ccd4f888baace0f2d4eb2dd9ad122f9a5d2
Jord279 <jordanbrooks2709@gmail.com>
2022-07-21 13:22:18 +0100

    Step 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import DisplayBlock from './Components/DisplayBlock';
+import PriceEntryField from './Components/PriceEntryField';
+import VATRateField from './Components/VATRateField';
 
 function App() {
+
+  const [netPrice, setNetPrice] = useState(0.0)
+  const [grossPrice, setGrossPrice] = useState(0.0)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="header field">   
+      VAT Calculator     
+      <div className="pale-green-border">
+        <VATRateField customstyle="field"/>
+        <PriceEntryField customstyle="field" label="Price Excl. VAT " price={netPrice === 0.0 ? "" : netPrice} />
+        <DisplayBlock customstyle="field" label="VAT to pay: " value="4"/>
+        <PriceEntryField customstyle="field" label="Price Incl. VAT " price={grossPrice === 0.0 ? "" : grossPrice}/>
+      </div>
     </div>
   );
 }
---
+++ src/Components/NumberEditorField.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const NumberEditorField = (props) => {
+    return (
+        <input type="number" id="num" value={props.value}/>
+    )
+}
+
+export default NumberEditorField
